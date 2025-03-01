b3449453f8831cb7d0121c3af9ff25b51f3ce14a
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-01 23:33:47 +0600

    Add files via upload

---
+++ Dart/HelloWorld.dart
@@ -0,0 +1,16 @@
+main() {
+  print("Hello World by Flutter");
+ // Variable
+  int number_1 = 20;
+  int number_2 = 30;
+  print("${number_1} ${number_2}"); //direct print
+
+  String name = "Neel";
+  print("Hello, $name!"); //concatenation and print
+ double number_3 = 10.20;
+ print(number_3); // Double number output
+ bool isTrue = false;
+ print(isTrue); //print boolean value
+
+  print("Number 1: ${number_1} | Number 2: ${number_2} | Boolean: ${isTrue} | Number 3: ${number_3} | Name: ${name}"); //concatenation and print
+   }
