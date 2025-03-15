1c8423bed7d5e6e49db929acc3a69e1565519ede
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-15 23:41:20 +0600

    Add files via upload

---
+++ Dart/Module 3/Loop.dart
@@ -0,0 +1,27 @@
+main(){
+  // for(int i=0; i<=10; i++){
+  //   print("Ami niche Desi $i times");
+  // }
+  // for(int i=0; i<=10; i++){
+  //   print("10 x $i = ${9*i}");
+  List <String> Student = ['Rahim', 'Toufik', 'dojo', 'riff'];
+
+  List <int> due = [100,200,400,500];
+
+  // print("Student 2 index is: ${Student[2]}");
+
+  // for(int index = 0; index<=Student.length; index++){
+  //   print("Student $index name: ${Student[index]} \nYour due: ${due[index]}");
+  // }
+
+  // for(var Student in Student){
+  //   int index = Student.indexOf(Student);
+  //   print("Student $index name: ${Student[index]} \nYour due: ${due[index]}");
+  // }
+
+  // Student.asMap().forEach((index,Student){
+  //   print("Student $index name: ${Student[index]} \nYour due: ${due[index]}");
+  // }
+
+
+}
---
+++ Dart/Module 3/While Loop.dart
@@ -0,0 +1,25 @@
+main(){
+  //int i  = 1;
+  //
+  // while (i<=10){
+  //   print('Hello world');
+  // i++;
+  // }
+  // while (i<=10){
+  //   print("15 x $i = ${15*(i)}");
+  //   i++;
+  // }
+  // List <String> student = ['Rahim', 'Toufik', 'dojo', 'riff'];
+  //
+  // List <int> due = [100,200,400,500];
+  // int index = 0;
+  // while (index< student.length){
+  //   print("${student[index]} Your due is ${due[index]}");
+  //   index++;
+  // }
+  int i =0;
+  do{
+    print('Hello $i times');
+    i++;
+  }while(i<=10);
+}
